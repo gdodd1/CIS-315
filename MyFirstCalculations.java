2362c81e88d930ad8917cc0f86e7668e12802e3e
gdodd1 <83469501+gdodd1@users.noreply.github.com>
2022-01-12 07:29:07 -0600

    Add Lab 1 - MyFirstCalculations

---
+++ MyFirstCalculations.java
@@ -0,0 +1,34 @@
+/*
+ Name: Garrett Dodd
+ Class: CIS 315
+ Professor: Dr. Herath
+ Date: 10 January 2022
+ Assignment: Lab 1 - My First Calculations 
+ */
+
+public class MyFirstCalculations {  
+   public static void main(String[] args) {  
+   
+ // Declare and initialize variables   
+  int    n1 = 20;   
+  int    n2 = 10;  
+  double n3 = 25.2;
+ // Process calculations   
+ int sum         = n1 + n2;   
+ int minus     = n1 - n2;   
+ int multiply = n1 * n2;   
+ int divide     = n1 / n2;  
+ int remainder = n1 % n2;  
+ double div1 = n3/n2;
+ double div2 = n1/n3;
+     
+ // Display output    
+  System.out.println("Sum is:         " + sum);  
+  System.out.println("Minus value is: " + minus);   
+  System.out.println("Multiplication: " + multiply);  
+  System.out.println("Divide:         " + divide);  
+  System.out.println("Remainder:      " + remainder);  
+  System.out.println("Divide 1:       " + div1);
+  System.out.println("Divide 2:       " + div2);
+  }  
+} 
